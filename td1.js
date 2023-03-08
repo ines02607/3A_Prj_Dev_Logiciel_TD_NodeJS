0a90ba7bf781556683a6a47d4b1c1f2824791c01
ines02607 <lina.mazouz@free.fr>
2023-03-08 21:26:48 +0100

    version finale

--- td1.js
+++ td1.js
@@ -9,115 +9,41 @@
  * @returns 
  */
 
-//appel des autres fichiers js
+// ===========================================
+// Appel des autres fichiers js
+// ===========================================
+
 const occurence = require("./occurence.js");
 
-/*
-function calculOccurence(tab, motcle)
-{
-// on crée un tableau de sortie vide
-	let tab2 = [];
-	// on utilise forEach pour chaque itération du tableau en entrée
-	tab.forEach((x)=> {
-	// On vérifie si il y a déjà un objet qui contient la valeur cherchée dans le tableau de sortie
-	
-		if(tab2.some((val)=>{ return val[motcle] == x[motcle] ;})
-		){
-			
-			// Si oui, on incrémente l'occurence de 1
-			tab2.forEach((k)=>{
-				if(k[motcle] === x[motcle])
-				{
-					k["occurrence"]++ ;
-				}
-			})
-			
-		}//test
-		else
-		// Sinon, on créé un nouvel objet dans le tableau et initialise l’objet avec le nom de la clé fournie
-		{
-		// et on initialise l'occurrence à 1
-			let a = {};
-			a[motcle] = x[motcle];
-			a["occurrence"] = 1 ;
-			tab2.push(a);
-		}
-	})
-	// on retourne le tableau de sortie avec noms des objets et occurences
-	return tab2
-}
-*/
-
-/*
-// = = = Lecture du fichier = = =
+// ===========================================
+// Lecture des fichiers
+// ===========================================
 
 // on définie fs pour la lecture du fichier json
 const fs = require("fs");
-
 // on vient lire le fichier users.json
 let rawdata = fs.readFileSync("users.json");
-
 // on manipule le fichier via cet objet nommé tab
 let tab = JSON.parse(rawdata);
 
-// = = = Argument à saisir = = = 
+// ===========================================
+// Saisi d'argument
+// ===========================================
+
 var arguments = process.argv; // on va ajouter l'argument à prendre en compte lors de la compilation
-
-let motcle = arguments;
-// soit country, soit company (au choix) à écrire après : node nom.js argument
-// on vient définir la clé à prendre en compte comme l'argument saisie lors de la compilation
-
-//var readlineSync = require('readline-sync'),
-//choix = ['Pays','Entreprises'],
-console.log('Que souhaitez-vous afficher ?');
-console.log('1 - Pays');
-console.log('2 -' );
-index = Number(readlineSync.question(""))
-//keyInSelect(choix, 'Que souhaitez-vous afficher ?');
-console.log();
-console.log('Ok, voici l affichage des ' + choix[index] + " et leur occurence !");
-
-if (index==1){
-	motcle = "country";
-}
-if (index==2) {
-	motcle = "company";
-}*/
-// = = = Tri décroissant = = = 
-
-
-
-// = = = Lecture du fichier = = =
-
-// on définie fs pour la lecture du fichier json
-const fs = require("fs");
-
-// on vient lire le fichier users.json
-let rawdata = fs.readFileSync("users.json");
-
-// on manipule le fichier via cet objet nommé tab
-let tab = JSON.parse(rawdata);
-
-// = = = Argument à saisir = = = 
-var arguments = process.argv; // on va ajouter l'argument à prendre en compte lors de la compilation
-
-// soit country, soit company (au choix) à écrire après : node nom.js argument
+// soit country, soit company (au choix)
 // on vient définir la clé à prendre en compte comme l'argument saisie lors de la compilation
 let motcle = arguments[2];
 
-//var readlineSync = require('readline-sync'),
-//choix = ['Pays','Entreprises'],
-//index = readlineSync.keyInSelect(choix, 'Que souhaitez-vous afficher ?');
-//console.log('Ok, voici l affichage des ' + choix[index] + ' et leur occurence !');
-
-
-// = = = Tri décroissant = = = 
+// ===========================================
+// Tri du tableau
+// ===========================================
 
 // on vient stocker le résultat de la fonction dans une variable result
 var result = occurence.occurence(tab, motcle);
-
 // on vient effectuer un tri inverse (b - a pour un tri décroissant // a - b pour un tri croissant)
 result.sort(function(a,b) {return b.occurrence - a.occurrence}); // on précise qu'on compare des occurences
-
 // on vient retourner le résultat trié dans l'ordre décroissant
 console.log(result);
+
+// COMPILATION : node nom.js argument
