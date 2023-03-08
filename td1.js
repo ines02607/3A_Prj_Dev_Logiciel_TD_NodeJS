14e66234becbfa6709465d886fe3a1e78a1f94b4
ines02607 <lina.mazouz@free.fr>
2023-03-08 21:34:40 +0100

    tentative menu

--- td1.js
+++ td1.js
@@ -10,13 +10,13 @@
  */
 
 // ===========================================
-// Appel des autres fichiers js
+// 		Appel des autres fichiers js
 // ===========================================
 
 const occurence = require("./occurence.js");
 
 // ===========================================
-// Lecture des fichiers
+// 			Lecture des fichiers
 // ===========================================
 
 // on définie fs pour la lecture du fichier json
@@ -27,7 +27,7 @@ let rawdata = fs.readFileSync("users.json");
 let tab = JSON.parse(rawdata);
 
 // ===========================================
-// Saisi d'argument
+// 				Saisi d'argument
 // ===========================================
 
 var arguments = process.argv; // on va ajouter l'argument à prendre en compte lors de la compilation
@@ -36,14 +36,39 @@ var arguments = process.argv; // on va ajouter l'argument à prendre en compte l
 let motcle = arguments[2];
 
 // ===========================================
-// Tri du tableau
+// 					Menu
 // ===========================================
 
-// on vient stocker le résultat de la fonction dans une variable result
-var result = occurence.occurence(tab, motcle);
-// on vient effectuer un tri inverse (b - a pour un tri décroissant // a - b pour un tri croissant)
-result.sort(function(a,b) {return b.occurrence - a.occurrence}); // on précise qu'on compare des occurences
-// on vient retourner le résultat trié dans l'ordre décroissant
-console.log(result);
+var readlineSync = require("readline-sync");
+
+console.log("\x1b[45m%s\x1b[0m", "\n Quel compteur voulez-vous voir ?");
+console.log("\x1b[36m%s\x1b[0m", "\n 1- Pays" );
+console.log("\x1b[36m%s\x1b[0m", " 2- Societe" );
+console.log("\x1b[45m%s\x1b[0m", "\n");
+
+const nb = Number(readlineSync.question(""));
+
+if (nb == 1)
+{
+    //Affichage à l'écran
+    console.log("  Pays et Compteur "); 
+	motcle = "country";
+	var result = occurence.occurence(tab, motcle);
+	// on vient effectuer un tri inverse (b - a pour un tri décroissant // a - b pour un tri croissant)
+	result.sort(function(a,b) {return b.occurrence - a.occurrence}); // on précise qu'on compare des occurences
+	// on vient retourner le résultat trié dans l'ordre décroissant
+	console.log(result);
+}
+else 
+{ 
+    //Affichage à l'écran
+	console.log("  Société et Compteur "); 
+	motcle = "company";
+	var result = occurence.occurence(tab, motcle);
+	// on vient effectuer un tri inverse (b - a pour un tri décroissant // a - b pour un tri croissant)
+	result.sort(function(a,b) {return b.occurrence - a.occurrence}); // on précise qu'on compare des occurences
+	// on vient retourner le résultat trié dans l'ordre décroissant
+	console.log(result);
+}
 
 // COMPILATION : node nom.js argument
